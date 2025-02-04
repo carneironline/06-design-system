9edfb3b97a2da7a1eb7af31f12b2cc16a2924068
Rodrigo Carneiro <carneironline@gmail.com>
2025-02-04 12:41:14 -0300

    Componente: Text

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,21 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Text, TextProps } from '@ignite-ui/react';
+
+export default {
+    title: 'Typography/Text',
+    component: Text,
+
+    args: {
+        children:
+            'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores ut necessitatibus temporibus similique voluptate modi, delectus quae rem expedita distinctio nobis culpa perspiciatis non dicta voluptates quis mollitia nostrum dignissimos?',
+    },
+} as Meta<TextProps>;
+
+export const Primary: StoryObj<TextProps> = {};
+
+export const CustomTag: StoryObj<TextProps> = {
+    args: {
+        as: 'strong',
+        children: 'Strong text',
+    },
+};
