a594d6388e0ce7439c1e17db84bdc8a7a77bacaf
Rodrigo Carneiro <carneironline@gmail.com>
2025-02-18 17:46:24 -0300

    Componente: TextInput

---
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -0,0 +1,47 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Box, Text, TextInput, TextInputProps } from '@ignite-ui/react';
+
+export default {
+    title: 'Form/Text Input',
+    component: TextInput,
+    tags: ['autodocs'],
+
+    args: {},
+    decorators: [
+        (storyFn) => {
+            return (
+                <Box
+                    as='label'
+                    css={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        gap: '$2',
+                    }}
+                >
+                    <Text size='sm'>E-mail address</Text>
+                    {storyFn()}
+                </Box>
+            );
+        },
+    ],
+} as Meta<TextInputProps>;
+
+type Story = StoryObj<TextInputProps>;
+
+export const Primary: Story = {
+    args: {
+        placeholder: 'Type your name',
+    },
+};
+
+export const Disabled: Story = {
+    args: {
+        disabled: true,
+    },
+};
+
+export const WithPrefix: Story = {
+    args: {
+        prefix: 'example.com/',
+    },
+};
