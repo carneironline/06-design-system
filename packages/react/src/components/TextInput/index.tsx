ad5f3e38ed75d72cdbf2203c699cc624a367ea71
Rodrigo Carneiro <carneironline@gmail.com>
2025-03-11 14:06:39 -0300

    refactor: remove props from TextInputProps

--- packages/react/src/components/TextInput/index.tsx
+++ packages/react/src/components/TextInput/index.tsx
@@ -3,8 +3,6 @@ import { Input, Prefix, TextInputContainer } from './styles';
 
 export interface TextInputProps extends ComponentProps<typeof Input> {
     prefix?: string;
-    placeholder?: string;
-    disabled?: boolean;
 }
 
 export const TextInput = forwardRef<ComponentRef<typeof Input>, TextInputProps>(
